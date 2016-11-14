86660007bb9c7ff1dfdf0a216ec8eb64cadfc439
alec-graves <alec0061@hotmail.com>
2016-11-13 20:25:08 -0500

    fix the urls and the script

--- url_update.sh
+++ url_update.sh
@@ -2,6 +2,7 @@
 rm -f underwater/url.txt
 touch url.txt
 for filename in underwater/*; do
-	echo "https://github.com/shadySource/DATA/raw/master/underwater"+"$filename" >> url.txt
+	echo "https://github.com/shadySource/DATA/raw/master/underwater/"${filename##*/} >> url.txt
 done
 mv url.txt underwater/
+cat underwater/url.txt
