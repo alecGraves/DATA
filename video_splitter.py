31f7c4bbfdd75823b8cc4eae0945236d1018607f
shadySource <shadySource2@gmail.com>
2017-07-28 09:57:08 -0700

    update splitting tools

--- video_splitter.py
+++ video_splitter.py
@@ -14,8 +14,8 @@ if debug:
 
 for vid in vids:
     if vid[-4:].lower() == ".mp4":
-        if not os.path.exists(vid[:-4]):
-            os.makedirs(vid[:-4])
-        os.system('ffmpeg -i ' + os.path.join(videoDir, vid)  + ' -s  640x480 -r 1 ' + os.path.join(vid[:-4], 'images%4d.jpg'))
+        if not os.path.exists("split_vids"):
+            os.makedirs("split_vids")
+        os.system('ffmpeg -i ' + os.path.join(videoDir, vid)  + ' -s  640x480 -r 1 ' + os.path.join("split_vids", vid[:-4]+'images%4d.jpg'))
 
 
